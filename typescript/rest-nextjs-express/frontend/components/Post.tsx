f395191845238a6845b315109d83273e482b861b
u-Hoshi <yuta000707@yahoo.co.jp>
2023-05-14 23:07:44 +0900

    くように修正

--- typescript/rest-nextjs-express/frontend/components/Post.tsx
+++ typescript/rest-nextjs-express/frontend/components/Post.tsx
@@ -1,31 +1,46 @@
 import React from 'react'
 import Router from 'next/router'
 import ReactMarkdown from 'react-markdown'
+import Image from 'next/image'
 
 export type PostProps = {
-  id: number;
-  title: string;
+  id: number
+  title: string
   author: {
-    name: string;
+    name: string
   }
-  content: string;
-  published: boolean;
+  content: string
+  published: boolean
 }
 
-const Post: React.FC<{post: PostProps}> = ({ post }) => {
+const Post: React.FC<{ post: PostProps }> = ({ post }) => {
   const authorName = post.author ? post.author.name : 'Unknown author'
   return (
-    <div onClick={() => Router.push('/p/[id]', `/p/${post.id}`)}>
-        <h2>{post.title}</h2>
-        <small>By {authorName}</small>
-        <ReactMarkdown children={post.content} />
-        <style jsx>{`
-          div {
-            color: inherit;
-            padding: 2rem;
-          }
-        `}</style>
-    </div>
+    <article className="sm:flex overflow-hidden rounded-lg shadow transition hover:shadow-lg dark:shadow-gray-700/25 basis-1/3">
+      <Image
+        alt="Office"
+        src="/hoge.png"
+        className="h-56 w-full object-cover"
+        width={240}
+        height={200}
+      />
+
+      <div className="bg-white p-4 dark:bg-gray-900 sm:p-6">
+        <a href="#">
+          <h3 className="mt-0.5 text-lg text-gray-900 dark:text-white">
+            How to position your furniture for positivity
+          </h3>
+        </a>
+
+        <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 dark:text-gray-400">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+          dolores, possimus pariatur animi temporibus nesciunt praesentium
+          dolore sed nulla ipsum eveniet corporis quidem, mollitia itaque minus
+          soluta, voluptates neque explicabo tempora nisi culpa eius atque
+          dignissimos. Molestias explicabo corporis voluptatem?
+        </p>
+      </div>
+    </article>
   )
 }
 
--- typescript/rest-nextjs-express/frontend/pages/index.tsx
+++ typescript/rest-nextjs-express/frontend/pages/index.tsx
@@ -7,20 +7,25 @@ type Props = {
   feed: PostProps[]
 }
 
-const Blog : React.FC<Props> = props => {
+const Blog: React.FC<Props> = (props) => {
   return (
     <Layout>
       <div className="page">
         <h1>My Blog</h1>
-        <main>
-          {props.feed.map(post => (
-            <div key={post.id} className="post">
+        <main className="flex flex-row">
+          {props.feed.map((post) => (
+            <div key={post.id} className=" basis-1/2">
               <Post post={post} />
             </div>
           ))}
         </main>
+        <div className="flex flex-row">
+          <div className="bg-green-100 px-2">1</div>
+          <div className="bg-green-100 px-2 ml-1">2</div>
+          <div className="bg-green-100 px-2 ml-1">3</div>
+        </div>
       </div>
-      <style jsx>{`
+      {/* <style jsx>{`
         .post {
           background: white;
           transition: box-shadow 0.1s ease-in;
@@ -33,7 +38,7 @@ const Blog : React.FC<Props> = props => {
         .post + .post {
           margin-top: 2rem;
         }
-      `}</style>
+      `}</style> */}
     </Layout>
   )
 }
